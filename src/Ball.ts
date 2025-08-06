0153aaf5119cdd56cb3a1ce9902b5e9f04869eb6
Lee Goddard <leegee@gmail.com>
2025-08-06 10:55:47 +0200

    screen boundary

--- src/Ball.ts
+++ src/Ball.ts
@@ -19,6 +19,30 @@ export class Ball {
     move() {
         this.x += this.vx;
         this.y += this.vy;
+
+        // Left boundary
+        if (this.x - this.radius < -this.renderer.canvas.width / 2) {
+            this.x = -this.renderer.canvas.width / 2 + this.radius;
+            this.vx = -this.vx;
+        }
+
+        // Right boundary
+        if (this.x + this.radius > this.renderer.canvas.width / 2) {
+            this.x = this.renderer.canvas.width / 2 - this.radius;
+            this.vx = -this.vx;
+        }
+
+        // Top boundary
+        if (this.y - this.radius < -this.renderer.canvas.height / 2) {
+            this.y = -this.renderer.canvas.height / 2 + this.radius;
+            this.vy = -this.vy;
+        }
+
+        // Bottom boundary
+        if (this.y + this.radius > this.renderer.canvas.height / 2) {
+            this.y = this.renderer.canvas.height / 2 - this.radius;
+            this.vy = -this.vy;
+        }
     }
 
     render() {
