7cc4a41be242e7ea85da1c5658a4b4051633f44e
Lee Goddard <leegee@gmail.com>
2025-08-06 11:19:53 +0200

    use ref

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,27 @@ import { initMidi } from './MIDI';
 import { Game } from './Game';
 
 export default function App() {
+    let canvasRef: HTMLCanvasElement | undefined;
+
     onMount(() => {
         initMidi('loop')
             .then(() => {
                 console.log("MIDI initialized");
-                const canvas = document.getElementById('hexCanvas') as HTMLCanvasElement;
-                const game = new Game(canvas);
-                game.start();
+                if (canvasRef) {
+                    const game = new Game(canvasRef);
+                    game.start();
+                }
             })
             .catch((e) => alert(e));
     });
 
     return (
         <>
-            <canvas id="hexCanvas" style="width: 100vw; height: 100vh; display: block;"></canvas>
+            <canvas
+                ref={canvasRef}
+                id="hexCanvas"
+                style="width: 100vw; height: 100vh; display: block;"
+            ></canvas>
         </>
     );
 }
